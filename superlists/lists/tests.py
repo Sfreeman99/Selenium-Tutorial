d13f781b3e9bd5b657f2a87d153257710cf81944
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2018-01-03 14:56:20 -0600

    Refactored homepage to use template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -14,5 +14,13 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         self.assertTrue(response.content.startswith(b"<html>"))
-        self.assertIn(b"<title> To-Do-lists </title>", response.content)
-        self.assertTrue(response.content.endswith(b"</html>"))
+        self.assertIn(b"<title>To-Do lists</title>", response.content)
+        self.assertTrue(response.content.strip().endswith(b"</html>"))
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+        # .decode is used to turn the page bytes into string syntax
+        # to compare strings
