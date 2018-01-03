1e02720598922afea462ab6245854707a5541892
Shedlia.Freeman <shedliaf@basecampcodingacademy.org>
2018-01-03 15:31:28 -0600

    Front Page Html now generated from a template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
